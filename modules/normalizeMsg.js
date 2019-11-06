86f54fd58cbd52cddb83b609e5163d36fc97a209
Gilson <gilson@abreu.eti>
2019-11-06 12:50:21 -0300

    changes from 'Chatbot v_3.docx' - Sala Ipês and more

--- modules/normalizeMsg.js
+++ modules/normalizeMsg.js
@@ -1,6 +1,8 @@
 function normalizeMsg(msg) {
     return msg && msg.trim().toLowerCase().normalize("NFD")
         .replace(/[\u0300-\u036f]/g, "")
-        .replace(/["'!@#$£%¢¨¬&\\*|()_\-+=§`´{}[\]ªº^~,<>.?/°;:]/g, "");
+        .replace(/["'!@#$£%¢¨¬&\\*|()_\-+=§`´{}[\]ªº^~,<>.?/°;:]/g, "")
+        .replace(/  +/g, " ")
+        ;
 }
 module.exports = normalizeMsg;
